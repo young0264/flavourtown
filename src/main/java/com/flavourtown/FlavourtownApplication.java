0fd679939ff39724979d0aef67efecb1a342b441
남의영 <ny2485@naver.com>
2022-10-13 16:49:11 +0900

    [REFACTOR] aws ec2 deploy

---
+++ src/main/java/com/flavourtown/FlavourtownApplication.java
@@ -0,0 +1,13 @@
+package com.flavourtown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavourtownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavourtownApplication.class, args);
+	}
+
+}
