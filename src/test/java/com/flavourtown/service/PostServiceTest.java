4ecbd051f7bd00592827c16675aebbcef55bf780
남의영 <ny2485@naver.com>
2022-11-12 21:45:35 +0900

    git ignore

---
+++ src/test/java/com/flavourtown/service/PostServiceTest.java
@@ -0,0 +1,15 @@
+package com.flavourtown.service;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @DisplayName("게시글 등록 test")
+    void createPost() {
+
+
+    }
+
+}
