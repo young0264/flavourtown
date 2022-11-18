0f98db56b43fc4ed20103ebac45cb479b6589e6c
남의영 <ny2485@naver.com>
2022-11-18 16:24:43 +0900

    [add] builder annotatioin add

--- src/main/java/com/flavourtown/web/dto/member/MemberInfoDto.java
+++ src/main/java/com/flavourtown/web/dto/member/MemberInfoDto.java
@@ -1,14 +1,16 @@
 package com.flavourtown.web.dto.member;
 
 import com.flavourtown.domain.member.MemberAge;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 @Getter @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberInfoDto {
 
     @NotBlank
