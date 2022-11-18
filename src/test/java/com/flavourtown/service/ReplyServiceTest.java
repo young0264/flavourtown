31595425b2d8b10795775bc2bfa9de12dfc2f8e7
남의영 <ny2485@naver.com>
2022-11-19 00:09:58 +0900

    [add] reply update test code

--- src/test/java/com/flavourtown/service/ReplyServiceTest.java
+++ src/test/java/com/flavourtown/service/ReplyServiceTest.java
@@ -7,6 +7,7 @@ import com.flavourtown.domain.reply.Reply;
 import com.flavourtown.domain.reply.ReplyRepository;
 import com.flavourtown.web.dto.reply.ReplyDto;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -39,9 +40,9 @@ class ReplyServiceTest {
     @Autowired
     ReplyService replyService;
 
+    Long postId;
 
-
-    @Test
+    @BeforeEach
     @DisplayName("댓글 등록")
     void t1() {
         ReplyDto replyDto = ReplyDto.builder()
@@ -49,10 +50,11 @@ class ReplyServiceTest {
                 .build();
 
         Post post = postRepository.findAll().get(0);
-        System.out.println("post id : " + post.getId());
+        postId = post.getId();
 
         Long replyId = replyService.saveReply(post.getId(), replyDto, post.getMember().getId());
         Reply reply = replyRepository.findById(replyId).orElse(null);
+
         assertThat(reply.getComment()).isEqualTo("test reply comment");
         assertThat(reply.getReplyLike().size()).isEqualTo(0);
         assertThat(reply.getModifyDate()).isNull();
@@ -67,12 +69,17 @@ class ReplyServiceTest {
     @Test
     @DisplayName("댓글 수정")
     void t2() {
+//        public void update(Reply reply, String content) {
+        Post post = postRepository.findById(postId).orElse(null);
+        Reply reply = post.getReplyList().get(0);
+        replyService.update(reply, "test update reply");
+        assertThat(reply.getComment()).isEqualTo("test update reply");
+        assertThat(reply.getModifyDate()).isNotNull();
+        assertThat(reply.getReplyLike().size()).isEqualTo(0);
+        }
 
-    }    @Test
-    @DisplayName("댓글 삭제")
-    void t3() {
 
-    }
+
 
 
 }
