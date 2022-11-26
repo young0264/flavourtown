6e0634edfeffc63bfb29d35b3ba5eb4d9415fe89
남의영 <ny2485@naver.com>
2022-11-26 18:07:42 +0900

    [bug] Member 객체 repository에서 찾아오기

--- src/main/java/com/flavourtown/web/controller/bookmark/FavoriteController.java
+++ src/main/java/com/flavourtown/web/controller/bookmark/FavoriteController.java
@@ -1,10 +1,12 @@
 package com.flavourtown.web.controller.bookmark;
 
 import com.flavourtown.domain.account.Account;
+import com.flavourtown.domain.account.AccountRepository;
 import com.flavourtown.domain.account.AuthUser;
 import com.flavourtown.domain.favorite.Favorite;
 import com.flavourtown.domain.member.Member;
 import com.flavourtown.domain.place.Place;
+import com.flavourtown.service.AccountService;
 import com.flavourtown.service.FavoriteService;
 import com.flavourtown.service.PlaceService;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +15,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
-import java.util.List;
 
 @Slf4j
 @Controller
@@ -21,17 +22,25 @@ import java.util.List;
 public class FavoriteController {
     private final FavoriteService favoriteService;
     private final PlaceService placeService;
+    private final AccountRepository accountRepository;
+    private final AccountService accountService;
 
     /**
      * 북마크 카테고리 추가
      */
     @PostMapping("/favorite/addFavorite")
     public String addFavorite(@AuthUser Account account, @PathParam("subject") String subject) {
-        Member member = account.getMember();
-        Favorite favorite = new Favorite(member, subject);
-        member.getFavoriteList().add(favorite); //연관관계
-        favorite.setMember(member);
-        favoriteService.save(favorite);
+        String username = account.getUsername();
+
+        //Member 객체 Repository에서 찾기
+        Account account1 = accountRepository.findByUsername(username).orElse(null);
+        Member member = account1.getMember();
+
+        Favorite favorite = favoriteService.initFavorite(member, subject);
+        System.out.println(member.getFavoriteList().size());// no session 이것도 에러남
+        favoriteService.addCategory(member,favorite);
+
+        favoriteService.addCategory(member, favorite);
         return "redirect:/profile/bookmark/view";
     }
 
@@ -54,24 +63,25 @@ public class FavoriteController {
     public String modifyFavorite(@AuthUser Account account, @PathVariable Long favoriteId, @PathParam("subject") String subject) {
         Member member = account.getMember();
         Favorite favorite = favoriteService.findByIdAndMember(favoriteId, member);
-        favorite.setSubject(subject);
+        favorite.insertSubject(subject);
         favoriteService.save(favorite);
         return "redirect:/profile/bookmark/view";
     }
 
     /**
-     *
+     *북마크 카테고리 삭제
      */
     @GetMapping("/favorite/deleteFavorite/{favoriteId}")
     public String deleteFavorite(@AuthUser Account account, @PathVariable Long favoriteId) {
         Member member = account.getMember();
         Favorite favorite = favoriteService.findByIdAndMember(favoriteId, member);
-        log.info("favorite.name -> {}",favorite.getSubject());
-
-        favoriteService.delete(favorite);
+        favoriteService.delete(member, favorite);
         return "redirect:/profile/bookmark/view";
     }
 
+    /**
+     * 북마크 장소 삭제
+     */
     @GetMapping("/favorite/{favoriteId}/deletePlace/{placeId}")
     public String deletePlace(@AuthUser Account account, @PathVariable Long favoriteId, @PathVariable Long placeId) {
         Member member = account.getMember();
