52c3e2735b8bbd646d763d0ac9067ebcede058b1
남의영 <ny2485@naver.com>
2022-11-29 22:48:21 +0900

    [add] 회원가입 account save test code

--- src/test/java/com/flavourtown/service/AccountServiceTest.java
+++ src/test/java/com/flavourtown/service/AccountServiceTest.java
@@ -1,14 +1,23 @@
 package com.flavourtown.service;
 
+import com.flavourtown.domain.account.Account;
+import com.flavourtown.domain.account.AccountRepository;
+import com.flavourtown.domain.account.LoginType;
+import com.flavourtown.web.dto.account.AccountSignUpDto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.transaction.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -21,20 +30,49 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestPropertySource(locations = "/application-test.properties")
 class AccountServiceTest {
 
-    @Test
-    @DisplayName("1")
-    void t1() {
+    @Autowired
+    private AccountService accountService;
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    private final String username = "test121";
+    private final String rowPwd = "test121!!";
+    private final String userEmail = "test@naver.com";
 
+
+//    public Account saveNewAccount(AccountSignUpDto accountSignUpDto, LoginType type) {
+    @Test//saveNewAccount
+    @DisplayName("account 회원가입")
+    void t1() {
+        AccountSignUpDto accountSignUpDto = AccountSignUpDto.builder()
+                .username(username)
+                .password(rowPwd)
+                .email(userEmail)
+                .build();
+        Account account = accountService.saveNewAccount(accountSignUpDto, LoginType.LOCAL);
+
+        //username, email, encoded password assert
+        assertThat("test121").isEqualTo(account.getUsername());
+        assertThat("test@naver.com").isEqualTo(account.getEmail());
+        assertTrue(passwordEncoder.matches(rowPwd,account.getPassword()));
     }
 
     @Test
-    @DisplayName("2")
+    @DisplayName("로그인")
     void t2() {
 
     }
 
-    @Test
+    @Test//getReadOnlyMember
+    @DisplayName("멤버 vo 가져오기")
     void t3() {
+
+    }
+
+    @Test//changeAccountPassword
+    @DisplayName("비밀번호 변경")
+    void t4() {
 
     }
 }
