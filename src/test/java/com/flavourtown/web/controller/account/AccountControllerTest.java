0444a69e54aba8da2824fc1c58a93569a86ee039
남의영 <ny2485@naver.com>
2022-11-30 21:37:20 +0900

    [add] Controller test, login get method

---
+++ src/test/java/com/flavourtown/web/controller/account/AccountControllerTest.java
@@ -0,0 +1,67 @@
+package com.flavourtown.web.controller.account;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import javax.transaction.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "/application-test.properties")
+class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("로그인페이지 보여주기")
+    void showLoginPage() throws Exception {
+        ResultActions resultActions = mockMvc
+                .perform(get("/login"))
+                .andExpect(status().isOk());
+
+    }
+//
+//    @Test
+//    void kakaoLogin() {
+//    }
+
+    @Test
+    void showSignUpPage() {
+
+    }
+
+    @Test
+    void createNewMember() {
+    }
+
+    @Test
+    void overlappedID() {
+    }
+
+    @Test
+    void memberInformationInit() {
+    }
+
+    @Test
+    void testCreateNewMember() {
+    }
+
+    @Test
+    void accountWithdrawal() {
+    }
+}
