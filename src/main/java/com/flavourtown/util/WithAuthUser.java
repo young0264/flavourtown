e804b557d8debf8c7aea178de153faa8758f9b2b
남의영 <ny2485@naver.com>
2022-12-05 21:08:38 +0900

    [add] 게시글생성 api test code

---
+++ src/main/java/com/flavourtown/util/WithAuthUser.java
@@ -0,0 +1,2 @@
+package com.flavourtown.util;public interface WithAuthUser {
+}
--- src/test/java/com/flavourtown/web/controller/post/PostControllerTest.java
+++ src/test/java/com/flavourtown/web/controller/post/PostControllerTest.java
@@ -1,32 +1,67 @@
 package com.flavourtown.web.controller.post;
 
+import com.flavourtown.domain.account.Account;
+import com.flavourtown.domain.account.AccountRepository;
+import com.flavourtown.service.AccountService;
+import com.flavourtown.util.WithAuthUser;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithSecurityContext;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import javax.transaction.Transactional;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Transactional
+@WebAppConfiguration
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "/application-test.properties")
 class PostControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    AccountService accountService;
+    @Autowired
+    AccountRepository accountRepository;
+//    @Autowired
+//    private WebApplicationContext context;
+//    @BeforeEach
+//    public void setup() {
+//        mockMvc = MockMvcBuilders
+//                .webAppContextSetup(context)
+//                .apply(springSecurity())
+//                .build();
+//    }
 
     @Test
     @DisplayName("/post/{id}/detail, get, 게시글 상세조회")
     void showDetailPost() throws Exception {
+
         mockMvc.perform(get("/post/87/detail"))
                 .andExpect(status().isOk());
     }
@@ -38,8 +73,31 @@ class PostControllerTest {
                 .andExpect(status().isOk());
     }
 
+    /**
+     * 로그인
+     */
+    @BeforeEach
+    @DisplayName("로그인")
+    void t0() throws Exception {
+
+
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface With{
+
+    }
+
+
     @Test
-    void newPost() {
+    @DisplayName("/post/new")
+//    @WithAnonymousUser
+//    @WithMockUser
+    void newPost() throws Exception {
+        Account account = accountRepository.findByUsername("22222").orElse(null);
+        accountService.login(account);
+        mockMvc.perform(get("/post/new"))
+                .andExpect(status().isOk());
     }
 
     @Test
